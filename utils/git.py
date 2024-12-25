4f925771aafb20326f74268fb72c5626e954556c
hiroki.sato <h.sato.0623@gmail.com>
2024-12-25 12:45:57 +0900

    feat: improve GitHub URL retrieval in utils/git.py
    Updated the get_github_url function to handle different URL schemes
    more effectively. The function now correctly processes both HTTPS and
    Git URLs, ensuring that the returned URL is always in the correct
    format. Additionally, it includes support for SSH URLs by converting
    them to the appropriate HTTPS format. This enhances the usability
    and flexibility of the function when working with various GitHub
    repository configurations.

--- utils/git.py
+++ utils/git.py
@@ -22,12 +22,15 @@ def get_git_root() -> str:
 def get_github_url() -> str:
     repo = git.Repo(search_parent_directories=True)
     remote_url = repo.remotes.origin.url
-    remote_url = remote_url.replace(".git", "")
     parsed_url = urlparse(remote_url)
-    if parsed_url.netloc == "github.com":
-        return f"https://github.com{parsed_url.path}"
+    if parsed_url.scheme == "https":
+        return remote_url.rstrip(".git")
+    elif parsed_url.scheme == "git":
+        return f"https://github.com{parsed_url.path}".rstrip(".git")
     else:
-        return remote_url.replace(".git", "")
+        # SSHの場合
+        ssh_path = parsed_url.path.split(":", 1)[-1]
+        return f"https://github.com/{ssh_path}".rstrip(".git")
 
 
 def get_commit_hash() -> str:
