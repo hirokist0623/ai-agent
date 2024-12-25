aa392477d63a9f5b1d5a7ab7942c143254497a23
hiroki.sato <h.sato.0623@gmail.com>
2024-12-25 12:55:31 +0900

    fix: remove unnecessary print statements in git.py
    This commit removes several print statements from the
    get_github_url and git_commit_url functions in git.py.
    These statements were primarily used for debugging purposes
    and are no longer needed, helping to clean up the code
    and improve readability.

--- utils/git.py
+++ utils/git.py
@@ -22,22 +22,14 @@ def get_git_root() -> str:
 def get_github_url() -> str:
     repo = git.Repo(search_parent_directories=True)
     remote_url = repo.remotes.origin.url
-    print(f"Remote URL: {remote_url}")
     parsed_url = urlparse(remote_url)
-    print(f"Remote URL: {parsed_url}")
     if parsed_url.scheme == "https":
-        print(f"parsed_url.path: {parsed_url.scheme}")
-        print(f"ssh_path: {remote_url}")
         return remote_url.replace(".git", "")
     elif parsed_url.scheme == "git":
-        print(f"parsed_url.scheme: {parsed_url.scheme}")
-        print(f"parsed_url.scheme: {parsed_url.path}")
         return f"https://github.com{parsed_url.path}".replace(".git", "")
     else:
         # SSHの場合
         ssh_path = parsed_url.path.split(":", 1)[-1]
-        print(f"parsed_url.path: {parsed_url.path}")
-        print(f"ssh_path: {ssh_path}")
         return f"https://github.com/{ssh_path}".replace(".git", "")
 
 
@@ -48,5 +40,4 @@ def get_commit_hash() -> str:
 
 def git_commit_url() -> str:
     commit_hash = get_commit_hash()
-    print(f"Commit URL: {get_github_url()}")
     return f"{get_github_url()}/commit/{commit_hash}"
